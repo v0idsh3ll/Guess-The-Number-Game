ba06aa3d7f5db1d80b260230401b68900992bb09
v0idsh3ll <v0idsh3ll@protonmail.com>
2025-10-28 13:16:47 +0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,30 @@
+import random #import the random library
+
+number = random.randint(1, 100) #generating random number
+
+def is_valid(number): #is the number valid?
+     if str(number).isdigit():
+         if int(number) >= 1 and int(number) <= 100:
+             return int(number)
+         else:
+             return False
+     else:
+        return False
+
+def inputing():
+    num = input("Type here your number: ")
+    while not is_valid(num):
+        print("You should type a number is bigger than 1 and smaller than 100")
+        num = input("Type here your number: ")
+    if is_valid(num):
+        return int(num)
+
+attempt = 0
+while attempt != number:
+    attempt = inputing()
+    if attempt < number:
+        print('Wished number is bigger than yours')
+    elif attempt > number:
+        print('Wished number is smaller than yours')
+    else:
+        print("You've guessed the number, congratulations!")
